ea3e632e21550e59a43e21e6f2c8d8f086171b35
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-09-28 11:34:48 +0200

    If Bedingungen

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+
+
+import java.util.Random;
+
 public class Main {
-    int number = 7;
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        System.out.println(randomNumber);
+        if (randomNumber<20){
+            System.out.println("Mini");
+        } else if (randomNumber>20 && randomNumber<50){
+            System.out.println("Medium");
+        } else if (randomNumber>50){
+            System.out.println("Large");
+        }
+    }
 }
