623cde8899e27bdc5c0b1349ad37d2875b29e5f7
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-10-03 11:59:12 +0200

    loop 1

--- src/loop1.java
+++ src/loop1.java
@@ -2,7 +2,7 @@ public class loop1 {
 
     public static void main(String[] args) {
         for (int i = 0; i < 101; i++) {
-            System.out.println(i);
+            System.out.println(i );
         }
     }
 }
