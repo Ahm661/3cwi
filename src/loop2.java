b05f441ee9c9bbfe2eb19f7c8e64a96d1cdaa73a
ahmet <ahmet.akbulut@student.htldornbirn.at>
2022-10-03 11:56:18 +0200

    Update

--- src/loop2.java
+++ src/loop2.java
@@ -1,8 +1,8 @@
 public class loop2 {
 
     public static void main(String[] args) {
-        for (int i = 2; i < 1001; i = i +2) {
-            System.out.println(i);
+        for (int i = 2; i < 1001; i = i + 2) {
+            System.out.println(i );
         }
     }
 }
