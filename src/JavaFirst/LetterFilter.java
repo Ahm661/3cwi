46eb8be457dfb16e2de4967a0875ed1b74f5cdac
ahmet <ahmet.akbulut@student.htldornbirn.at>
2023-02-02 08:49:40 +0100

    CodeinClash

---
+++ src/JavaFirst/LetterFilter.java
@@ -0,0 +1,25 @@
+package JavaFirst;
+
+import java.util.Scanner;
+
+public class LetterFilter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Geben Sie einen Text ein: ");
+        String input = sc.nextLine();
+        String upperCase = "";
+        String lowerCase = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isUpperCase(c)) {
+                upperCase += c;
+            } else if (Character.isLowerCase(c)) {
+                lowerCase += c;
+            }
+        }
+
+        System.out.println("Großbuchstaben: " + upperCase);
+        System.out.println("Kleinbuchstaben: " + lowerCase);
+    }
+}
