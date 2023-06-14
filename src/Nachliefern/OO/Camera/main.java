08563509263c59fd4cc4c8de5819269ecfedd3a7
ahmet <ahmet.akbulut@student.htldornbirn.at>
2023-06-14 23:25:19 +0200

    Kamera Augfgabe nachgeliefert

--- src/Nachliefern/OO/Camera/main.java
+++ src/Nachliefern/OO/Camera/main.java
@@ -6,7 +6,7 @@ public class main {
         ManufacturerCamera mc1 = new ManufacturerCamera("Canon","Austria");
         ManufacturerLens ml1 = new ManufacturerLens("Hubu", "Indonesia");
         Lens l1 = new Lens(250, ml1);
-        Camera c1 = new Camera(140,12,"blue", Camera.RESOLUTION.s,s1,l1, mc1);
+        Camera c1 = new Camera(140,12,"green", Camera.RESOLUTION.s,s1,l1, mc1);
 
 
         c1.takePic();
